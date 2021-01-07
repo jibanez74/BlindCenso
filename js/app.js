85d549178ddd92dfb85278893f40f2ffef58d5b1
jibanez74 <jibanez74@gmail.com>
2021-01-07 00:06:09 -0400

    finished with ui

---
+++ js/app.js
@@ -0,0 +1,9 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const censoForm = document.querySelector('#censo-form');
+
+  censoForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    alert('Este formulario todavía está en desarrollo');
+  });
+});
