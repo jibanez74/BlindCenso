c50c909ec6c51f05a5efe94040ef7bc332fcffc7
jibanez74 <jibanez74@gmail.com>
2021-01-10 18:57:21 -0400

    working on js alerts and loaders

--- js/app.js
+++ js/app.js
@@ -1,9 +1,52 @@
 window.addEventListener('DOMContentLoaded', () => {
   const censoForm = document.querySelector('#censo-form');
+  const sendEmailBtn = document.querySelector('#btn-email');
+
+  sendEmailBtn.addEventListener('click', () => {
+    alert('Esta funcionalidad todavía no ha sido implementada');
+    return;
+  });
 
   censoForm.addEventListener('submit', function (e) {
     e.preventDefault();
 
-    alert('Este formulario todavía está en desarrollo');
+    $('#btn-submit').hide();
+    $('#loader').show();
+
+    const data = new FormData(this);
+
+    const name = document.querySelector('#name').value;
+    const fathername = document.querySelector('#fathername').value;
+    const mothername = document.querySelector('#mothername');
+    const edad = document.querySelector('#edad').value;
+    const email = document.querySelector('#email').value;
+    const phone = document.querySelector('#phone').value;
+    const pueblo = document.querySelector('#pueblo').value;
+    const edu = document.querySelector('#nivel-edu').value;
+    const condition = document.querySelector('#condition').value;
+
+    if (!email || !phone) {
+      $('#danger')
+        .show()
+        .html('Debe probeer un número de teléfono, o su email o ambos');
+
+      setTimeout(() => {
+        $('#danger').hide();
+      }, 5000);
+
+      $('#loader').hide();
+      $('#btn-submit').show();
+      return;
+    } else {
+      $('#success').show();
+
+      setTimeout(() => {
+        $('#success');
+      }, 5000);
+    }
+
+    $('#censo-form').reset();
+
+    return;
   });
 });
