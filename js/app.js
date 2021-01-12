fb93e8dbf5329280d30a7b9f2f3de9fac3a37ff8
jibanez74 <jibanez74@gmail.com>
2021-01-12 11:57:54 -0400

    added alerts and effects to form

--- js/app.js
+++ js/app.js
@@ -1,52 +1,43 @@
-window.addEventListener('DOMContentLoaded', () => {
-  const censoForm = document.querySelector('#censo-form');
-  const sendEmailBtn = document.querySelector('#btn-email');
-
-  sendEmailBtn.addEventListener('click', () => {
-    alert('Esta funcionalidad todavía no ha sido implementada');
-    return;
-  });
-
-  censoForm.addEventListener('submit', function (e) {
+$(document).ready(() => {
+  $('#censo-form').on('submit', function (e) {
     e.preventDefault();
 
     $('#btn-submit').hide();
-    $('#loader').show();
 
-    const data = new FormData(this);
+    const name = $('#name').val();
+    const fathername = $('#fathername').val();
+    const mothername = $('#mothername').val();
+    const email = $('#email').val();
+    const phone = $('#phone').val();
+    const education = $('#nivel-edu').val();
+    const location = $('#pueblo').val();
+    const age = $('#edad').val();
+    const condition = $('#condition').val();
 
-    const name = document.querySelector('#name').value;
-    const fathername = document.querySelector('#fathername').value;
-    const mothername = document.querySelector('#mothername');
-    const edad = document.querySelector('#edad').value;
-    const email = document.querySelector('#email').value;
-    const phone = document.querySelector('#phone').value;
-    const pueblo = document.querySelector('#pueblo').value;
-    const edu = document.querySelector('#nivel-edu').value;
-    const condition = document.querySelector('#condition').value;
+    if (!email && !phone) {
+      $('#messages')
+        .html('<p> Debe probeer su email, número de teléfono o ambos')
+        .addClass('alert alert-danger')
+        .fadeIn();
 
-    if (!email || !phone) {
-      $('#danger')
-        .show()
-        .html('Debe probeer un número de teléfono, o su email o ambos');
-
-      setTimeout(() => {
-        $('#danger').hide();
-      }, 5000);
-
-      $('#loader').hide();
       $('#btn-submit').show();
-      return;
-    } else {
-      $('#success').show();
 
       setTimeout(() => {
-        $('#success');
+        $('#messages').fadeOut().html().removeClass('alert alert-danger');
       }, 5000);
+    } else {
+      $(this).fadeOut();
+      $('#btn-submit').hide();
+      $('#messages')
+        .addClass('alert alert-success')
+        .html(
+          '<p> Su formulario ha sido sometido.  Gracias por su participación. </p>'
+        )
+        .fadeIn();
+
+      setTimeout(() => {
+        window.location.href = 'censo.html';
+      }, 8000);
     }
-
-    $('#censo-form').reset();
-
-    return;
   });
 });
